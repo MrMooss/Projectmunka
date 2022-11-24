f1f860ae5be4a72b31a27ece50330c075f932fd4
Szabó Attila <szattila1998@gmail.com>
2022-11-24 22:46:00 +0100

    VGG, Ui, SR fix

--- SuperResolution.py
+++ SuperResolution.py
@@ -1,18 +1,22 @@
+import GAN
 import os
 import cv2
 import numpy as np
 from keras.models import load_model
+from keras.layers import Input
 
-generator = load_model('gen_e_20.h5', compile=False)
 
-HR = None
-
+shape = (32, 32, 3)
+ip = Input(shape=shape)
+generator = GAN.create_gen(ip, num_res_block = 16)
+generator.load_weights('gen_e_20.h5')
 
 def generateHr(path):
     img = convertImage(path)
-    #highres = generator.predict(img)
-    global HR
-    HR = img
+    cv2.imwrite('img.jpeg', img)
+    highres = generator.predict(img)
+    cv2.imwrite('high.jpeg', highres)
+    return highres
 
 
 def convertImage(path):
--- Ui.py
+++ Ui.py
@@ -9,10 +9,10 @@ import cv2
 
 class QImageViewer(QMainWindow):
 
-    path = " "
-
     def __init__(self):
         super().__init__()
+
+        self.path = ''
 
         self.imageLabel = QLabel()
         self.imageLabel.setBackgroundRole(QPalette.Light)
@@ -66,24 +66,20 @@ class QImageViewer(QMainWindow):
 
         self.updateActions()
 
-    def superRes(self, path):
-        if path != " ":
-            sr.generateHr(path)
-            breakpoint()
-            print(sr.HR.shape)
-            x, h, w, ch = sr.HR.shape
-            bytes_per_line = ch * w
-            convert_to_Qt_format = QtGui.QImage(sr.HR.data, w, h, bytes_per_line, QtGui.QImage.Format_RGB888)
-            p = convert_to_Qt_format.scaled(self.disply_width, self.display_height, Qt.KeepAspectRatio)
+    def superRes(self):
+        if self.path != '':
+            img = sr.generateHr(self.path)
+            p = QImage(img, img.shape[1], img.shape[0], img.strides[0], QtGui.QImage.Format_RGB888)
             self.imageLabel.setPixmap(QPixmap.fromImage(p))
             self.scaleFactor = 1.0
+            self.normalSize()
 
             if not self.fitToWindowAct.isChecked():
                 self.imageLabel.adjustSize()
 
     def createActions(self):
         self.openAct = QAction("&Open...", self, shortcut="Ctrl+O", triggered=self.open)
-        self.srAct = QAction("&Enhance", self, enabled=False, triggered=lambda: self.superRes(self.path))
+        self.srAct = QAction("&Enhance", self, enabled=False, triggered=self.superRes)
         self.exitAct = QAction("E&xit", self, shortcut="Ctrl+Q", triggered=self.close)
         self.normalSizeAct = QAction("&Normal Size", self, shortcut="Ctrl+S", enabled=False, triggered=self.normalSize)
         self.fitToWindowAct = QAction("&Fit to Window", self, enabled=False, checkable=True, shortcut="Ctrl+F",
